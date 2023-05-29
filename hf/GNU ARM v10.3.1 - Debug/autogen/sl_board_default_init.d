b6eeed4a168cfa3a98ce61f4a89f770800487cf8
Dominik Papp <dominikpapp@Dominiks-Air.home>
2023-05-29 12:10:40 +0200

    Added ESC function and fixed bugs

--- hf/GNU ARM v10.3.1 - Debug/autogen/sl_board_default_init.d
+++
@@ -1,9 +0,0 @@
-autogen/sl_board_default_init.o: ../autogen/sl_board_default_init.c \
- /Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//hardware/board/inc/sl_board_control.h \
- /Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_status.h \
- /Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_enum.h
-/Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//hardware/board/inc/sl_board_control.h:
-/Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_status.h:
-/Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_enum.h:
--- hf/GNU ARM v10.3.1 - Debug/gecko_sdk_4.2.3/platform/common/src/sl_string.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.2.3/platform/common/src/sl_string.o: \
- /Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk/platform/common/src/sl_string.c \
- /Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_string.h
-/Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/Users/dominikpapp/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_string.h:
